4aac4955f717bce8dde3f9bad8e9cc0e292ece3b
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-27 13:05:06 -0500

    customized Django admin interface

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
-
-admin.site.unregister(Group)
+from django.contrib.auth.models import User, Group
+from .models import Profile
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #Only display the "username" field
+    fields = ["username"]
+    inlines = [ProfileInline]
+    
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
