1668740447e883aca1bba651a1483274bcbe4eda
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-28 09:23:45 -0500

    access individual profiles that display following and followers

--- dwitter/views.py
+++ dwitter/views.py
@@ -10,3 +10,8 @@ def dashboard(request):
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "dwitter/profile.html", {"profile": profile})
