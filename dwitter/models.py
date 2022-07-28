9cf37ef00bf8c59e0de33b296d001d8408263a16
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-28 14:00:34 -0500

    created dwitter model

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,9 +1,16 @@
-from http.client import ImproperConnectionState
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+@receiver(post_save, sender=User)        
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()       
+        
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -19,12 +26,9 @@ class Profile(models.Model):
         return self.user.username
     
     
-@receiver(post_save, sender=User)        
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        user_profile = Profile(user=instance)
-        user_profile.save()
-        user_profile.follows.add(instance.profile)
-        user_profile.save()            
-        
-        
+class Dweet(models.Model):
+    user = models.ForeignKey(
+        User, related_name="dweets", on_delete=models.DO_NOTHING
+    )        
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
