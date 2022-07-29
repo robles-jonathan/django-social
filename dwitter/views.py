dc58a68960fa2e1e3235b0cfdb78d3f3aaf76643
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-29 09:26:47 -0500

    sorted out dweets in views

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import DweetForm
-from .models import Profile
+from .models import Dweet, Profile
 
 # Create your views here.
 def dashboard(request):
@@ -11,7 +11,13 @@ def dashboard(request):
             dweet.user = request.user
             dweet.save()
             return redirect("dwitter:dashboard")
-    return render(request, "dwitter/dashboard.html", {"form": form})
+        
+    followed_dweets = Dweet.objects.filter(
+        user__profile__in=request.user.profile.follows.all()
+    ).order_by("-created_at")
+    return render(request, "dwitter/dashboard.html", 
+                {"form": form, "dweets": followed_dweets},
+                )
 
 
 def profile_list(request):
